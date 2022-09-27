d596184be49c98b70cff319f11d98862d3d1845b
Joe P Shoulak <joe@squarecompass.io>
2022-09-26 19:34:44 -0500

    Update and Create are working

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,5 @@
 // Global state variable -- this feels dirty
-var taskBeingEdited = null;
-
-var modal = $("#updateTaskModal");
+var taskBeingEditedID = null;
 
 // Test data
 var testTask = {
@@ -9,8 +7,8 @@ var testTask = {
     title: "Testing",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
 
-    dueDate: moment().format("MM/DD/YYYY"),
-    weatherDependant: true,
+    dueDate: moment().format("YYYY-MM-DD"),
+    weather: true,
     completed: false,
 }
 
@@ -19,8 +17,8 @@ var testTask2 = {
     title: "Testing 2",
     description: "Wassup Wassup Wassup Wassup Wassup.",
 
-    dueDate: moment().format("MM/DD/YYYY"),
-    weatherDependant: true,
+    dueDate: moment().format("YYYY-MM-DD"),
+    weather: true,
     completed: false,
 }
 
@@ -31,16 +29,35 @@ function getFormData() {
     return {
         title: $("#form-title").val(),
         description: $("#form-description").val(),
+        id: taskBeingEditedID,
     
         dueDate: $("#form-due-date").val(),
-        weather: $("#form-weather").val(),
-        completed: $("#form-completed").val(),
+        weather: $("#form-weather").is(":checked"),
+        completed: $("#form-completed").is(":checked"),
     };
 }
 
 // Get the object id from an HTML element containing the data for a task object
 function getIdFromTaskElement(taskElement) {
     return taskElement.find(".id")[0].innerHTML;
+}
+
+function autofillForm(data) {
+    $("#form-title").val(data.title);
+    $("#form-description").val(data.description);
+
+    $("#form-due-date").val(data.dueDate);
+    $("#form-weather").prop('checked', data.weather);
+    $("#form-completed").prop('checked', data.completed);
+}
+
+function clearForm() {
+    $("#form-title").val("");
+    $("#form-description").val("");
+
+    $("#form-due-date").val("");
+    $("#form-weather").prop('checked', false);
+    $("#form-completed").prop('checked', false);
 }
 
 // The callback function for clicking a button on a task
@@ -52,7 +69,12 @@ function taskClicked(event) {
 
     // If we're updating a task...
     if (button.text() == "Update") {
-        taskBeingEdited = getIdFromTaskElement(taskElement); // Keep track of what we're editing
+        taskBeingEditedID = getIdFromTaskElement(taskElement); // Keep track of what we're editing
+
+        var data = taskList.find(e => e.id == taskBeingEditedID);
+
+        autofillForm(data);
+
 
         /* The rest of the task is updated by interacting with the modal that is 
            automatically opened by clicking on any update button. When clicking 
@@ -65,22 +87,45 @@ function taskClicked(event) {
 function updateTask() {
     var data = getFormData(); // Grab the updated data from the form modals
 
-    // Grab the index where the ID matches what we're editing
-    var taskListIndex = taskList.findIndex(e => e.id == taskBeingEdited);
+    // Grab the list index where the ID matches what we're editing
+    var taskListIndex = taskList.findIndex(e => e.id == taskBeingEditedID);
 
+    console.log(taskList[taskListIndex] )
     taskList[taskListIndex] = data; // Update the data where it sits
+    console.log(taskList[taskListIndex] )
 
     displayAllTasks(); // Re-render everything
+}
+
+function newTask() {
+    // Set the new ID to one more than the max of all current IDs
+    taskBeingEditedID = Math.max(...taskList.map(t => t.id)) + 1;
+
+    clearForm();
+
+    taskList.push({
+        id: taskBeingEditedID,
+    })
 }
 
 // Event Listeners
 $("#task-list").on("click", taskClicked);
 $("#form-submit").on("click", updateTask);
+$("#new-task").on("click", newTask);
+
+$(".close-modal").on("click", function() {
+    console.log(taskList)
+    
+    var trash = taskList.pop(); 
+    
+    console.log(taskList)
+})
 
 // Render a task by appending it to the DOM with all the correct elements
 function renderTask(task) {
     return $("<div>").addClass("task card column col-4").append(
         $("<div>").addClass("card-body")
+
             // Title and description
             .append($("<h5>").addClass("tcard-title").text(task.title))
             .append($("<p>").text(task.description))
@@ -92,7 +137,12 @@ function renderTask(task) {
 
             // Weather
             .append($("<label>").text("Weather-Dependant:")
-                .append($("<span>").text(task.weatherDependant))
+                .append($("<span>").text(task.weather))
+            )
+
+            // Due Date 
+            .append($("<label>").text("Due Date:")
+                .append($("<span>").text(task.dueDate))
             )
 
             // Completed? 
@@ -102,7 +152,12 @@ function renderTask(task) {
             
             // Buttons for Mark as Complete, Update, and Delete
             .append($("<button>").addClass("btn btn-primary").text("Complete"))
-            .append($("<button>").addClass("btn btn-secondary").text("Update").attr("data-mdb-toggle","modal").attr("data-mdb-target", "#updateTaskModal"))
+            .append($("<button>")
+                .addClass("btn btn-secondary")
+                .text("Update")
+                .attr("data-mdb-toggle","modal")
+                .attr("data-mdb-target", "#updateTaskModal")
+            )
             .append($("<button>").addClass("btn btn-secondary").text("Delete"))
     )
 }
@@ -122,4 +177,5 @@ function init() {
     displayAllTasks();
 }
 
+// Run this once when the page first loads
 init();
